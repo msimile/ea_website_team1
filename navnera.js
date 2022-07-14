34948340069c2e9a1b29b04abca0c9f4c08f85d0
Ivan <ivanfrasca92@gmail.com>
2022-07-14 21:18:40 +0200

    remove rules from Header section, gave them to nav-nera section

--- navnera.js
+++ navnera.js
@@ -1,19 +1,19 @@
 const navIcons = document.querySelector(".nav-icons");
 const children = [].slice.call(navIcons.children);
 const icons = navIcons.children;
-const header = document.querySelector("header");
+const navNera = document.querySelector(".nav-nera");
 let actualPos = null;
 window.addEventListener("scroll", (e) => {
   let windowScroll = window.scrollY;
   if (windowScroll > actualPos) {
-    header.classList.add("header-invisible");
+    navNera.classList.add("header-invisible");
     children.forEach((element) =>
       element.classList.add("header-invisible", "li-invisible")
     );
     navIcons.classList.add("header-invisible");
     console.log(icons);
   } else {
-    header.classList.remove("header-invisible");
+    navNera.classList.remove("header-invisible");
     console.log(icons);
     children.forEach((element) => element.classList.remove("header-invisible"));
     navIcons.classList.remove("header-invisible");
